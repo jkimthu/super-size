a3b42ee95a6eb5cd711d8e67333898c2c2d57beb
Jen Nguyen <jknguyen@mit.edu>
2020-02-11 16:22:41 +0100

    extend simulations to steady conditions with measured mu, keeping tau function same as t=15 and 60

--- figure6_simulate_growth_rate_fluc.m
+++ figure6_simulate_growth_rate_fluc.m
@@ -15,8 +15,9 @@
 %            environments, using distribution of tau measured for each Vi
 
 
-%  Last edit: jen, 2020 Jan 20
-%  commit: add simulations of growth and division with fixed tau
+%  Last edit: jen, 2020 Feb 11
+%  commit: extend simulations to steady conditions with measured mu,
+%          keeping same tau functions as T = 15 and 60 min approx.
 % 
 
 
@@ -38,13 +39,7 @@ specificColumn = 3;
 % 0. initialize complete meta data
 cd('/Users/jen/Documents/StockerLab/Data_analysis/')
 load('storedMetaData.mat')
-exptArray = [9:15]; % list experiments by index
-
-
-% 0. initialize colors per successive period
-%colorSpectrum = {'Indigo','MediumSlateBlue','DodgerBlue','DeepSkyBlue','Teal','DarkGreen','MediumSeaGreen','GoldenRod','DarkOrange','Red'};
-colorSpectrum = {'Indigo','DodgerBlue'};%,'DeepSkyBlue','Teal','MediumSeaGreen','GoldenRod','Gold'};
-%colorSpectrum = {'SlateGray','DarkCyan','CadetBlue','DeepSkyBlue','DodgerBlue','Navy','Indigo',};
+exptArray = 9:15; % list experiments by index
 
 %% B. loop through each experiment and isolate growth rates
 
@@ -168,9 +163,19 @@ clear growthRt_binned growthRt isEnd period_current
 clear startTime specificColumn date filename exptCounter exptArray
 clear timeinPeriodFraction timestamps_final ee counter15 counter60
 
-save
+
+% 12. save measured mu functions 
+save('mu_functions','signals_15','signals_60')
 
 %% C. compile mean growth rate signal and approx. timestep vector
+
+clear
+clc
+
+% 0. initialize stored mu functions
+load('mu_functions')
+colorSpectrum = {'Indigo','DodgerBlue'};
+
 
 % 1. mu(t) for each timescale
 mufun15 = mean(signals_15,2);
@@ -182,7 +187,7 @@ figure(1) % mean of replicate means
 plot(1:length(mufun15),mufun15,'Color',rgb(colorSpectrum(2)),'LineWidth',1,'Marker','.')
 hold on
 plot(1:length(mufun60),mufun60,'Color',rgb(colorSpectrum(1)),'LineWidth',1,'Marker','.')
-ylabel('growth rate: log2 (1/hr)')
+ylabel('mu (1/h)')
 xlabel('time bin (1/30th period time)')
 
 clear signal_e exp_type
@@ -648,3 +653,112 @@ for tau = .5:0.1:1
     
 end
 legend('0.5','0.6','0.7','0.8','0.9','1')
+
+%% I. simulate cells with steady G_low, G_ave and G_high
+
+% uses same tau function as T = 15 and 60 min conditions
+% manually vary Vi to demonstrate that same steady state is reached
+% regardless of initial condition.
+
+clear
+clc
+
+% 0. initialize steady-state growth rates from steady environments
+G_steady = [1.07, 2.31, 2.86]; % low, ave, high
+
+
+% 0. initialize simulation parameters
+gen = 1000;
+tstep = 15/3600; % timestep for each timescale
+
+% 0. initialize vectors for data storage
+tau_G = nan(gen,length(G_steady));
+Vd_G = nan(gen,length(G_steady));
+Vb_G = nan(gen,length(G_steady));
+
+
+for gg = 1:length(G_steady)
+    
+    % 0. initialize steady-state G for current condition
+    currentG = G_steady(gg);
+    
+    % 0. start generation counter
+    cycle = 1;
+    
+    % 0. initiation size, timestep, tau
+    new_cell = 1; % 0 = division did not just happen
+    Vi = 5;
+    tau = -(17/60)*Vi + (64/60);
+    t_cc = 0; % time counter for cell cycle
+    V = [];
+    t = [];
+    
+    counter_loop = 0; % each loop is another timestep
+    while cycle < gen+1
+        
+        counter_loop = counter_loop + 1;
+        
+        if new_cell == 1
+            % store current cell cycle Vi
+            Vb_G(cycle,gg) = Vi;
+        end
+        
+        % calculate exponent
+        muT = currentG * tstep; % mu(t)*t in hours
+        
+        % calculate new volume after timestep
+        if muT < 0
+            Vt = Vi;
+        else
+            Vt = Vi * 2^(muT);
+        end
+        
+        % calculate time into cell cycle
+        t_cc = t_cc + tstep;
+        
+        % store timestep data
+        V(counter_loop) = Vt;
+        if counter_loop == 1
+            t(counter_loop) = tstep;
+        else
+            t(counter_loop) = t(counter_loop-1) + tstep;
+        end
+        
+        % determine whether cell divides
+        
+        if t_cc >= tau
+            
+            % division! store V_div and tau data
+            Vd_G(cycle,gg) = Vt;
+            tau_G(cycle,gg) = tau;
+            
+            % re-set parameters for next cell cycle
+            cycle = cycle + 1;
+            Vi = Vt/2;
+            tau = -(17/60)*Vi + (64/60);
+            new_cell = 1;
+            t_cc = 0;
+            
+        else
+            Vi = Vt;
+            new_cell = 0;
+        end
+        
+    end
+    
+    figure(6)
+    hold on
+    plot(t,V)
+    axis([0 30 0 6])
+    xlabel('Time (h)')
+    ylabel('Volume')
+    title('Vi = 2')
+    
+    clear ans counter_loop experimentFolder expType
+    clear minTime new_cell period_frac specificGrowthRate
+    clear index growthRates_final muT t t_cc tau
+    clear timeInPeriodFraction V Vi Vt
+    
+end
+legend('Glow','Gave','Ghigh')
+
